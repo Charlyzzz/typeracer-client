6140b315afde07c6f369758950e7bd0bb978b793
Erwin Debusschere <erwincdl@gmail.com>
2019-12-29 21:14:37 -0300

    extracts goroutines

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"os"
 	"time"
 
-	proto "github.com/Charlyzzz/typeracer-client/grpc"
+	proto "github.com/Charlyzzz/typeracer-client/typeracer"
 	"github.com/eiannone/keyboard"
 	"google.golang.org/grpc"
 )
@@ -16,77 +16,84 @@ const (
 	address = "10.0.1.5:8080"
 )
 
+func listenKeyStrokes() (chan keyboard.Key, error) {
+	strokes := make(chan keyboard.Key)
+	err := keyboard.Open()
+	if err != nil {
+		return nil, err
+	}
+	go func() {
+		defer close(strokes)
+		for {
+			_, key, err := keyboard.GetKey()
+			if err != nil {
+				log.Fatalf("keyboard get key failed %v", err)
+			}
+			strokes <- key
+		}
+	}()
+	return strokes, nil
+}
+
+func pushMetrics(name string, stream proto.TypeRacer_SendPlayerMetricsClient) chan int {
+	metrics := make(chan int)
+	go func() {
+		defer close(metrics)
+		for {
+			m := <-metrics
+			metric := &proto.PlayerMetrics{Username: name, StrokesPerMinute: int32(m)}
+			err := stream.Send(metric)
+			if err != nil {
+				log.Fatalf("send player metrics failed %v", err)
+			}
+		}
+	}()
+	return metrics
+}
+
 func main() {
 	args := os.Args[1:]
 	if len(args) == 0 {
-		log.Panicf("Name is required!")
+		log.Fatalf("name is required!")
 	}
 	name := args[0]
 
 	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+		log.Fatalf("did not connect %v", err)
 	}
-	defer conn.Close()
 	c := proto.NewTypeRacerClient(conn)
-
-	err2 := keyboard.Open()
-	if err2 != nil {
-		panic(err2)
-	}
-	defer keyboard.Close()
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*1000)
-	defer cancel()
+	ctx := context.Background()
 	stream, err := c.SendPlayerMetrics(ctx)
 	if err != nil {
-		log.Fatalf("could not greet: %v", err)
+		log.Fatalf("could not send player metrics %v", err)
 	}
-	exit := make(chan struct{})
+	metrics := pushMetrics(name, stream)
 	go func() {
 		for {
 			in, err := stream.Recv()
 			if err == io.EOF {
-				close(exit)
+				log.Fatalf("server closed stream")
 			}
 			if err != nil {
-				log.Fatalf("Failed to receive a note : %v", err)
+				log.Fatalf("scoreboard receive failed %v", err)
 			}
 			log.Printf("Got message %s", in.Reply[0].Username)
 		}
 	}()
-	strokes := make(chan struct{})
-	go func() {
-		for {
-			_, key, err := keyboard.GetKey()
-			if err != nil {
-				panic(err)
-			} else if key == keyboard.KeyCtrlC {
-				close(exit)
-			}
-			strokes <- struct{}{}
-		}
-	}()
-	metrics := make(chan int)
-	go func() {
-		for {
-			m := <-metrics
-			metric := &proto.PlayerMetrics{Username: name, StrokesPerMinute: int32(m)}
-			stream.Send(metric)
-		}
-	}()
-	strokeCount := 0
+	strokes, err := listenKeyStrokes()
+	if err != nil {
+		log.Fatalf("keyboard open error %v", err)
+	}
+	var strokeCount int
 	ticker := time.NewTicker(500 * time.Millisecond)
 	for {
 		select {
 		case <-strokes:
-			strokeCount += 1
+			strokeCount += 2 * 60
 		case <-ticker.C:
-			metrics <- strokeCount * 2 * 60
+			metrics <- strokeCount
 			strokeCount = 0
-		case <-exit:
-			stream.CloseSend()
-			os.Exit(0)
 		}
 	}
 }
